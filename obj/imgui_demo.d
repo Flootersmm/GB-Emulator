5955a21926ba468c14490535ce367d95d659122a
Flootersmm <conrad.clough@hotmail.com>
2024-07-31 23:59:08 +0200

    Cleaned build artifacts

--- obj/imgui_demo.d
+++
@@ -1,2 +0,0 @@
-obj/imgui_demo.o: src/imgui_demo.cpp include/dear_imgui/imgui.h \
- include/dear_imgui/imconfig.h
--- obj/imgui_impl_glfw.d
+++
@@ -1,2 +0,0 @@
-obj/imgui_impl_glfw.o: src/imgui_impl_glfw.cpp include/dear_imgui/imgui.h \
- include/dear_imgui/imconfig.h include/dear_imgui/imgui_impl_glfw.h
